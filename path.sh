6a0ef73415d88f5ade7759e9c0cdf9ab63ab3689
Tarek Ramadan <tramadan.up@gmail.com>
2021-12-03 14:51:07 +0100

    solution on abstraction

---
+++ path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clingo -c horizon=4 asprilo-encodings/shortest_path/pathfinder.lp instances/abstraction1/instance.lp | head -n1 > instances/abstraction1/plan.lp
+
+#clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 -c horizon=4 asprilo-encodings/shortest_path/pathfinder.lp instances/abstraction1/instance.lp | head -n1 > instances/abstraction1/plan.lp
